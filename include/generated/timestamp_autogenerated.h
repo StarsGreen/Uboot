7a4a047115109a693c4be9eac894f1fa978604f4
greenstar <781770440@qq.com>
2020-03-14 22:17:03 +0800

    add jz2440 support

--- include/generated/timestamp_autogenerated.h
+++
@@ -1,4 +0,0 @@
-#define U_BOOT_DATE "Jan 11 2020"
-#define U_BOOT_TIME "20:19:14"
-#define U_BOOT_TZ "+0800"
-#define U_BOOT_DMI_DATE "01/11/2020"
--- include/generated/version_autogenerated.h
+++
@@ -1,4 +0,0 @@
-#define PLAIN_VERSION "2016.01"
-#define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
-#define CC_VERSION_STRING "gcc (Ubuntu 4.8.4-2ubuntu1~14.04.4) 4.8.4"
-#define LD_VERSION_STRING "GNU ld (GNU Binutils for Ubuntu) 2.24"
